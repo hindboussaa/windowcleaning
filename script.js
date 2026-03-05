94e2269358006c76b70e56a7f26677767fd7cde1
hindboussaa <116459428+hindboussaa@users.noreply.github.com>
2026-03-05 04:29:42 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function scrollContact() {
+
+document.getElementById("contact").scrollIntoView({
+behavior: "smooth"
+});
+
+}
+
+function callBusiness(){
+
+window.location.href = "tel:01234567890";
+
+}
